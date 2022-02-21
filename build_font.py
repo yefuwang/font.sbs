9abcffa62cb82f4f72ae3b69f8530f4ac86c1e55
Yefu Wang <m@yefu.org>
2022-02-20 20:34:04 -0800

    update: now with all glyphs

--- build_font.py
+++ build_font.py
@@ -29,11 +29,21 @@ font.generate(f'{name}.woff2')
 # print samples
 fontforge.printSetup('pdf-file', 'z.pdf', 600, 200)
 font.selection.select(("unicode","ranges"),ord('A'),ord('Z'))
-sample_text = """abcdefghijklmnokqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ
-1lI oO0 1234567890 .,:;!?'"
-%*+-=~<>/\(){}
-   6
-AAA6
+sample_text = """
+
+ 	!	"	#	$	%	&	'	(	)	*	+	,	-	.	/
+0	1	2	3	4	5	6	7	8	9	:	;	<	=	>	?
+@	A	B	C	D	E	F	G	H	I	J	K	L	M	N	O
+P	Q	R	S	T	U	V	W	X	Y	Z	[	\	]	^	_
+`	a	b	c	d	e	f	g	h	i	j	k	l	m	n	o
+p	q	r	s	t	u	v	w	x	y	z	{	|	}	~	
+ 	¡	¢	£	¤	¥	¦	§	¨	©	ª	«	¬	­	®	¯
+°	±	²	³	´	µ	¶	·	¸	¹	º	»	¼	½	¾	¿
+À	Á	Â	Ã	Ä	Å	Æ	Ç	È	É	Ê	Ë	Ì	Í	Î	Ï
+Ð	Ñ	Ò	Ó	Ô	Õ	Ö	×	Ø	Ù	Ú	Û	Ü	Ý	Þ	ß
+à	á	â	ã	ä	å	æ	ç	è	é	ê	ë	ì	í	î	ï
+ð	ñ	ò	ó	ô	õ	ö	÷	ø	ù	ú	û	ü	ý	þ	ÿ
+
 import fontforge
 from pathlib import Path
 
